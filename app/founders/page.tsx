295cc73fa7b376e1a73034157732c1c4f1c3e123
Rachit Kumar Tiwari <rachit.17.01.2004@gmail.com>
2026-03-26 22:32:52 +0530

    Update page.tsx

--- app/founders/page.tsx
+++ app/founders/page.tsx
@@ -11,7 +11,7 @@ const founders = [
   {
     name: "Rachit Kumar Tiwari",
     role: "Founder & Developer",
-    image: "/images/Rachit_tiwari.jpg",
+    image: "/images/rachit.jpg",
     linkedin: "https://www.linkedin.com/in/rachitkrtiwari/",
   },
   {
